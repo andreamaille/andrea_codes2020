6949c55ae1d0a08b3f4e68c2b84a769ca0dfbdf8
andreamaille <andrea.maille@gmail.com>
2020-08-12 20:08:18 -0400

    Initial set-up and SVG testing

---
+++ js/app.js
@@ -0,0 +1,19 @@
+import '../styles/style.scss';
+import gsap from 'gsap';
+
+const pathArray = document.querySelectorAll('path');
+
+// gsap.set(path, { strokeDasharray: 400, strokeDashoffset: 205 });
+
+// gsap.fromTo(path, 3, { strokeDashoffset: 205 }, { strokeDashoffset: 0 });
+
+// TweenMax.set(path, { strokeDasharray: l });
+// TweenMax.fromTo(path, 3, { strokeDashoffset: l }, { strokeDashoffset: 0 });
+pathArray.forEach(path => {
+  console.log(path);
+  const l = path.getTotalLength();
+
+  gsap.set(path, { strokeDasharray: l });
+
+  gsap.fromTo(path, 3, { strokeDashoffset: l }, { strokeDashoffset: 0 });
+});
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  mode: process.env.NODE_ENV || 'production',
+  devtool: 'source-map',
+  entry: './js/app.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|eot|svg|gif|woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
