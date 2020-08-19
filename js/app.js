396cb06291a56548ba65fc6aa45956cce02bbb62
andreamaille <andrea.maille@gmail.com>
2020-08-18 23:47:10 -0400

    Updates to header and added animations

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ app.hamburger = () => {
 };
 
 app.svgAnimation = () => {
-  const svgPaths = document.querySelectorAll('path');
+  const svgPaths = document.querySelectorAll('header path');
 
   svgPaths.forEach(path => {
     const length = path.getTotalLength();
@@ -33,6 +33,13 @@ app.svgAnimation = () => {
     gsap.set(path, { strokeDasharray: length });
     gsap.fromTo(path, 5, { strokeDashoffset: length }, { strokeDashoffset: 0 });
   });
+};
+
+app.shadowAnimation = () => {
+  setTimeout(() => {
+    const headerContainer = document.querySelector('.header_main-content');
+    headerContainer.classList.add('shadow-pop-br');
+  }, 2000);
 };
 
 app.smoothScroll = () => {
@@ -43,12 +50,51 @@ app.smoothScroll = () => {
   });
 };
 
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function() {
+    const context = this;
+    const args = arguments;
+    const later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide() {
+  sliderImages.forEach(sliderImage => {
+    // half way through the image
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      console.log('true');
+      sliderImage.classList.add('active');
+    } else {
+      console.log('false');
+      sliderImage.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
+
 app.init = () => {
   app.svgAnimation();
+  app.shadowAnimation();
   app.hamburger();
   app.smoothScroll();
-
-  const svgPaths = document.querySelectorAll('path');
+  // app.slideImages();
 };
 
 app.init();
